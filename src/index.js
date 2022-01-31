94287a1ef555e86edb10b57e2d913b42a2ae6384
Yaroslav <kznkv.test@gmail.com>
2022-01-31 20:47:37 +0200

    add absolute imports and App component

--- src/index.js
+++ src/index.js
@@ -1,36 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = () => {
-    return <h1>Hello App component </h1>
-}
-
-const Text = () => {
-    return (
-        <>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Consectetur eos totam similique commodi, cumque amet veritatis
-                animi? Facilis voluptatem nulla ipsam, ab quidem consectetur.
-                Facere voluptates eos possimus rerum accusamus?
-            </p>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Consectetur eos totam similique commodi, cumque amet veritatis
-                animi? Facilis voluptatem nulla ipsam, ab quidem consectetur.
-                Facere voluptates eos possimus rerum accusamus?
-            </p>
-        </>
-    )
-}
-
-const App = () => {
-    return (
-        <>
-            <Header />
-            <Text />
-        </>
-    )
-}
+import App from 'components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
